59c16a5b6eec7f0783f5756435c4a4f28968edcc
Sebastian Alfaro <sebastianealfaro@gmail.com>
2022-10-04 09:28:54 -0300

    fix: textarea fix 2

--- common/components/CustomTextarea/CustomTextArea.js
+++ common/components/CustomTextarea/CustomTextArea.js
@@ -6,9 +6,9 @@ const CustomTextArea = (props) => {
     <>
       <div className="textareaBox">
         <label htmlFor="my-custom-textarea">{props.label}</label>
-        <textarea name="my-custom-textarea" rows="10" cols="50" {...props}/>
+        <textarea name="my-custom-textarea" {...props}/>
       </div>
-      <style jsx>
+      {/* <style jsx>
         {`
           .textareaBox{
             display: flex;
@@ -34,7 +34,7 @@ const CustomTextArea = (props) => {
             }
           }
         `}
-      </style>
+      </style> */}
     </>
   )
 }
